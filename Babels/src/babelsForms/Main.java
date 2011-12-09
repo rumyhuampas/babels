e967d0c2e0b5d7ca2a1ecad86e671b5812e1fd59
rumyhuampas <rumyhuampas@gmail.com>
2011-12-09 06:44:26 +0000

    First upload for user management

--- Babels/src/babelsForms/Main.java
+++ Babels/src/babelsForms/Main.java
@@ -10,7 +10,24 @@ public class Main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        mbarMain = new javax.swing.JMenuBar();
+        mitemFile = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        mitemUsers = new javax.swing.JMenu();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        mitemFile.setText("Archivo");
+
+        jMenuItem1.setText("jMenuItem1");
+        mitemFile.add(jMenuItem1);
+
+        mbarMain.add(mitemFile);
+
+        mitemUsers.setText("Usuarios");
+        mbarMain.add(mitemUsers);
+
+        setJMenuBar(mbarMain);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -20,11 +37,15 @@ public class Main extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 279, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuBar mbarMain;
+    private javax.swing.JMenu mitemFile;
+    private javax.swing.JMenu mitemUsers;
     // End of variables declaration//GEN-END:variables
 }
